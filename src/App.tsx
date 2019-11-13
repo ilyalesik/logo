218a5c4082fc1fdcf6820cf105fd9788626dfd65
Ilia Lesik <ilialesik@gmail.com>
2019-11-13 14:47:27 +0300

    Create logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import * as React from 'react';
-import { Page, Rectangle, Text } from 'react-figma';
+import { Page, View, Frame, StyleSheet } from 'react-figma';
+
+const styles = StyleSheet.create({
+    line: {
+        flexDirection: ("row" as any)
+    },
+    rect: {
+        width: 150,
+        height: 100
+    }
+});
 
 export const App = () => {
     return (
-        <Page isCurrent name="Page X">
-            <Text characters="Hello, react-figma!"/>
-            <Rectangle style={{ width: 200, height: 100, backgroundColor: '#0ddd25' }} />
-            <Rectangle style={{ width: 200, height: 100, backgroundColor: '#a6dd00' }} />
+        <Page isCurrent name="Logo page">
+            <Frame name="Logo">
+                <View name="Line 1" style={styles.line}>
+                    <View name="1.1" style={([styles.rect, { backgroundColor: '#f24e1e' }] as any)} />
+                    <View name="1.2" style={([styles.rect, { backgroundColor: '#ff7262' }] as any)} />
+                </View>
+                <View name="Line 2" style={styles.line}>
+                    <View name="2.1" style={([styles.rect, { backgroundColor: '#a259ff' }] as any)} />
+                    <View name="2.2" style={([styles.rect, { backgroundColor: '#1abcfe' }] as any)} />
+                </View>
+                <View name="Line 3" style={styles.line}>
+                    <View name="3.1" style={([styles.rect, { backgroundColor: '#0acf83' }] as any)} />
+                </View>
+            </Frame>
         </Page>
     );
 };
